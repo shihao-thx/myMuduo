1db61d2bd0b5d54ed4fc7642150f8e540a35a04c
shihao-thx <shihao@zju.edu.cn>
2022-12-07 18:38:02 +0800

    rm muduo dir

--- muduo/net/poller/DefaultPoller.cc
+++
@@ -1,9 +0,0 @@
-#include "muduo/net/Poller.h"
-#include "muduo/net/poller/EpollPoller.h"
-
-using namespace muduo;
-using namespace muduo::net;
-
-Poller* Poller::newDefaultPoller(EventLoop* loop) {
-    return new EpollPoller(loop);
-}
